a9239c86977989504186183e037f1288f3126c22
Richele <168489035+devrichele@users.noreply.github.com>
2024-05-07 20:34:05 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Boa noite galera :)</h1>
+      <h2>Estou aprendendo React!</h2>
+    </div>
+  );
+}
